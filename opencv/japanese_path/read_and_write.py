d6099c743cd5bb9e5dbac0ee37ed7454c3e67adb
catlover <stak@polluce.local>
2022-10-30 18:36:33 +0900

    assertion error

--- opencv/japanese_path/read_and_write.py
+++ opencv/japanese_path/read_and_write.py
@@ -6,29 +6,24 @@ import numpy as np
 
 def imread(filename, flags=cv2.IMREAD_COLOR):
     """imread for path containing Japanese character."""
-    try:
-        n = np.fromfile(filename, dtype=np.uint8)
-        img = cv2.imdecode(n, flags)
-        return img
-    except Exception as e:
-        print(e)
-        return False
+    assert Path(filename).exists(), '{} does not exist!'.format(filename)
+
+    img_arr = np.fromfile(filename, dtype=np.uint8)
+    img = cv2.imdecode(img_arr, flags)
+
+    return img
 
 
 def imwrite(filename, img, params=None):
     """imwrite for path containing Japanese characters."""
     try:
-        ext = Path(filename).suffix
-        res, n = cv2.imencode(ext, img, params)
-        if res:
-            with open(filename, mode='w+b') as f:
-                n.tofile(f)
-            return True
-        else:
-            return False
-    except Exception as e:
-        print(e)
-        return False
+        _, img = cv2.imencode(Path(filename).suffix, img, params)
+    except cv2.error:
+        print('The image cannot be encoded into {}!'.format(filename))
+        exit(1)
+
+    with open(filename, mode='w+b') as f:
+        img.tofile(f)
 
 
 if __name__ == '__main__':
