a108df5384ccc35076c51e96cb0da1f537e5d692
catlover <stak@polluce.local>
2022-11-05 17:48:55 +0900

    Add sort by number

---
+++ toolbox/sort_by_number.py
@@ -0,0 +1,23 @@
+def sort_by_number(filelist, prefix, suffix):
+    # remove prefix and suffix, and sort by number
+    newlist = sorted(
+        filelist, key=lambda f: int(f.replace(prefix, '').replace(suffix, '')))
+
+    return newlist
+
+
+if __name__ == '__main__':
+    prefix = 'hoge'
+    suffix = 'fuga'
+    filelist = []
+    for i in range(1000):
+        filelist.append(prefix + str(i) + suffix)
+
+    # naive sort
+    filelist = sorted(filelist)
+    print('Built-in sort')
+    print(filelist)
+
+    filelist = sort_by_number(filelist, prefix, suffix)
+    print('Number sort')
+    print(filelist)
